df35480ff67200312e32ad805f0d59b3c93096f2
Lukas Galke <git@lpag.de>
2016-10-30 02:19:54 +0200

    proposal schedule, python starting point

--- retrieval.py
+++ retrieval.py
@@ -3,16 +3,36 @@ from sklearn.base import TransformerMixin
 from sklearn.neighbors.base import NeighborsBase, KNeighborsMixin
 from sklearn.neighbors import NearestNeighbors
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import make_pipeline, Pipeline
-from scipy.sparse import vstack
+from sklearn.pipeline import Pipeline
 import scipy.sparse as sp
 import numpy as np
+import gist.rank_metrics as rm
+from sklearn.metrics import f1_score
 
+def InverseTermMatch(q, X):
+    # X (terms, documents)
+    # q (terms, 1)
+    terms = q.nonzero()[0]
+    indices = np.unique(X[terms, :].nonzero()[1])
+    return indices
 
 def TermMatch(q, X):
+    # X (documents, terms)
+    # q (1, terms)
     indices = np.unique(X.transpose()[q.nonzero()[1], :].nonzero()[1])
-    print(indices)
     return indices
+
+def _checkXy(X, y):
+    if y is not None and len(X) != len(y):
+        raise ValueError("Shapes of X and y do not match.")
+
+
+def average_ndcg_at_k(rs, k, method=0):
+    return np.mean([rm.ndcg_at_k(r, k, method) for r in rs])
+
+VALID_METRICS = {"mean_reciprocal_rank": rm.mean_reciprocal_rank,
+                 "mean_average_precision": rm.mean_average_precision,
+                 "average_ndcg_at_k": average_ndcg_at_k}
 
 
 class RetrievalModel(NeighborsBase, KNeighborsMixin, TransformerMixin):
@@ -20,12 +40,13 @@ class RetrievalModel(NeighborsBase, KNeighborsMixin, TransformerMixin):
     # delegate NearestNeighbor thingy
 
     def __init__(self, vectorizer=TfidfVectorizer(), metric='cosine',
-                 algorithm='brute', match_fn=TermMatch, **kwargs):
+                 algorithm='brute', match_fn=TermMatch, verbose=0, **kwargs):
         """ initializes vectorizer and passes remaining params down to
         NeighborsBase
         """
         self.vectorizer = vectorizer
         self.match_fn = match_fn
+        self.verbose = verbose
         self._init_params(metric=metric,
                           algorithm=algorithm,
                           **kwargs)  # NeighborsBase
@@ -34,20 +55,26 @@ class RetrievalModel(NeighborsBase, KNeighborsMixin, TransformerMixin):
         """ Fit the vectorizer and transform X to setup the index,
         if y is given, copy it and return its corresponding values
         on later queries. Consider y as the documents' ids """
-        if y is not None and len(X) != len(y):
-            raise ValueError("If you provide y make sure its length equals X")
+        _checkXy(X, y)
         self._X = self.vectorizer.fit_transform(X)
-        self._y = np.array(y) if y is not None else None
+        if y is None:
+            n_docs = self._X.shape[0]
+            self._y = np.arange(n_docs)
+        else:
+            self._y = np.asarray(y)
         return self
 
     def partial_fit(self, X, y=None):
         """ Add some objects into the index """
-        if self._y is not None:
-            if y is not None and len(X) != len(y):
-                raise ValueError("Shapes...")
-            self._y = sp.vstack([self._y, y])
+        _checkXy(X, y)
         Xt = self.vectorizer.transform(X)
         self._X = sp.vstack([self._X, Xt])
+        if y is None:
+            next_id = np.amax(self._y) + 1
+            new_ids = np.arange(next_id, next_id + Xt.shape[0])
+            self._y = np.hstack([self._y, new_ids])
+        else:
+            self._y = np.hstack(self._y, np.asarray(y))
 
         return self
 
@@ -64,37 +91,48 @@ class RetrievalModel(NeighborsBase, KNeighborsMixin, TransformerMixin):
             self.partial_fit(X, y)
 
     def query(self, X, k=1, **kwargs):
-        print("Query started:", X)
-        print("self._X.shape:", self._X.shape)
-        if self._y is not None:
-            print("self._y.shape:", self._y.shape)
         Xquery = self.transform(X)
-        print("Xquery.shape:", Xquery.shape)
-        # 'matching' operation TODO make this variable
-        # match_fn :: Xquery -> _X -> indices for X (and y)
+        results = []
         for x in Xquery:
-            print("x.shape:", x.shape)
             if self.match_fn is not None:
                 row_mask = self.match_fn(x, self._X)
-                if self._y is not None:
-                    Xm, ym = self._X[row_mask], self._y[row_mask]
-                else:
-                    Xm = self._X[row_mask]
+                Xm, ym = self._X[row_mask], self._y[row_mask]
             else:
                 Xm, ym = self._X, self._y
-
-            print("Xm.shape:", Xm.shape)
-            if self._y is not None:
-                print("ym.shape:", ym.shape)
-
             self._fit(Xm)  # NeighborsBase
             n_ret = min(Xm.shape[0], k)  # dont retrieve more than available
             ind = self.kneighbors(x, n_neighbors=n_ret, return_distance=False)
-            if self._y is not None:
-                labels = np.choose(ind, ym)
-                yield labels
-            else:
-                yield ind
+            labels = np.choose(ind, ym)
+            results.append(labels.ravel())
+        return np.asarray(results)
+
+    def score(self, X, Y, k=20, metrics=VALID_METRICS.keys()):
+        """ Y: relevancy table of shape (n_queries, n_samples) with relevancy\
+        values"""
+        assert Y.shape == (len(X), self._X.shape[0])
+        rs = []
+        for qid, result in enumerate(self.query(X, k)):
+            r = Y[qid, result]
+            rs.append(r)
+
+        rs = np.asarray(rs)
+        print("DEBUG rs:", rs)
+        values = {}
+        if "average_ndcg_at_k" in metrics:
+            values["average_ndcg_at_k"] = average_ndcg_at_k(rs, k)
+        if "mean_reciprocal_rank" in metrics:
+            values["mean_reciprocal_rank"] = rm.mean_reciprocal_rank(rs)
+        if "mean_average_precision" in metrics:
+            values["mean_average_precision"] = rm.mean_average_precision(rs)
+
+        # print(rs)
+        # nz = np.count_nonzero(rs)
+        # y = sp.csr_matrix((np.ones()), shape=Y.shape)
+        # y[rs] = 1
+        # print(y)
+        # values["f1_score"] = f1_score(Y, y)
+
+        return values
 
 
 def TfIdfRetrieval():
--- retrieval_test.py
+++ retrieval_test.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python3
 from retrieval import RetrievalModel, TermMatch
+import pprint
 if __name__ == '__main__':
     tfidf = RetrievalModel()
     corpus = [
         "Some information is in this document, let's see if it gets retrieved",
-        "The quick brown fox jumps over the lazy dog"
+        "The quick brown fox jumps over the lazy dog",
+        "fox doc"
     ]
     print("Testing RetrievalModel without document ids...")
     tfidf.fit(corpus)
-    newdoc = "Additional information about fox"
+    newdoc = "Additional quick information about fox"
     corpus.append(newdoc)
     print("Testing partial fit...")
     tfidf.partial_fit([newdoc])
     print(tfidf)
     QUERYTERMS = ["fox quick", "information retrieval"]
-    for results in tfidf.query(QUERYTERMS):
-        print(*[corpus[result] for result in results], sep='\n')
-
     print("Testing RetrievalModel with document ids...")
     X, y = zip(*[(doc, docid) for docid, doc in enumerate(corpus)])
     tfidf.fit(X, y)
-    for results in tfidf.query(QUERYTERMS):
-        print(*results, sep='\n')
+    for query, result in zip(QUERYTERMS,tfidf.query(QUERYTERMS, 3)):
+        print("QUERY:",query)
+        print(result)
+        for docid in result:
+            print(docid,corpus[int(docid)])
+    print("TESTING SCORING!!!")
+    import numpy as np
+    rels = np.array([[0,1,0,1],[1,0,0,0]])
+    print(rels.shape)
+    scores = tfidf.score(QUERYTERMS, rels, k=20)
+    pprint.pprint(scores)
+
