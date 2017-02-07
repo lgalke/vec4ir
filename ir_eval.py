31410b3b8241734c2c639867a27b4f5bf16d563a
Lukas Galke <git@lpag.de>
2017-02-07 20:22:17 +0100

    matchfix

--- ir_eval.py
+++ ir_eval.py
@@ -208,6 +208,9 @@ def _ir_eval_parser(config):
     matching_options.add_argument("-S", "--dont-stop", dest='stop_words',
                                   default=True, action='store_true',
                                   help="Do NOT use stopwords")
+    matching_options.add_argument("-M", "--no-matching", dest='matching', 
+                                  default=True, action='store_false',
+                                  help="Do NOT apply matching operation.")
 
     parser.add_argument("-u", "--train", default=False, action='store_true',
                         help="Train a whole new word2vec model")
