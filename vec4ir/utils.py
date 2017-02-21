188d75f3f31ab608a5c3b0b7c68708f4097475a4
Lukas Galke <git@lpag.de>
2017-02-21 13:47:34 +0100

    argtopk rework

--- vec4ir/utils.py
+++ vec4ir/utils.py
@@ -22,35 +22,47 @@ def filter_vocab(model, words, oov=None):
 
 
 def argtopk(A, k, axis=-1, sort=True):
-    """
-    >>> A = [5,4,3,6,7,8,9,0]
+    """ Get the the top k elements (in sorted order)
+    >>> A = np.asarray([5,4,3,6,7,8,9,0])
     >>> argtopk(A, 3)
     array([6, 5, 4])
     >>> argtopk(A, 1)
     array([6])
-    >>> argtopk(A, -3)
-    array([7, 2, 1])
-    >>> argtopk(A, -1)
-    array([7])
-    >>> argtopk(A, -6)
-    array([7, 2, 1, 0, 3, 4])
     >>> argtopk(A, 6)
     array([6, 5, 4, 3, 0, 1])
     >>> argtopk(A, 10)
     array([6, 5, 4, 3, 0, 1, 2, 7])
+    >>> argtopk(A, 28)
+    array([6, 5, 4, 3, 0, 1, 2, 7])
+    >>> B = np.asarray([[1,2,3],[4,3,2],[-9,-2,-7]])
+    >>> B.shape
+    (3, 3)
+    >>> r = argtopk(B, 2)
+    >>> r[0]
+    array([2,1])
+    >>> r[1]
+    array([0,1])
+    >>> r[2]
+    array([1,2])
     """
-    k = min(len(A), k)
     A = np.asarray(A)
-    if k == 0:
-        raise UserWarning("k <= 0? result [] may be undesired.")
-        return []
+    if k is None or k >= len(A):
+        # catch this for more convenience, if list is too short
+        return np.argsort(A, axis=axis)[::-1]
+
+    assert k > 0
+    # now 0 < k < len(A)
     ind = np.argpartition(A, -k, axis=axis)
-    ind = ind[-k:] if k > 0 else ind[:-k]
+    import sys
+    # select k highest, so fancy for multi dimensional arrays
+    ind = ind[..., -k:]  # <++TODO++> only works for single dim
+    print(ind, file=sys.stderr)
 
     if sort:
-        ind = ind[np.argsort(A[ind])]
-        if k > 0:
-            ind = ind[::-1]
+        # sort according to values in A
+        ind = ind[np.argsort(A[ind], axis=axis)]
+        # argsort is always from lowest to highest, so reverse
+        ind = ind[::-1]
 
     return ind
 
