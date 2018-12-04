e8e81792f90d018703d3ef5950579cf6c991ef86
Lukas Galke <git@lpag.de>
2018-12-04 11:16:32 +0100

    Fix non-matching query in tests

--- tests/test_query_expansion.py
+++ tests/test_query_expansion.py
@@ -42,7 +42,7 @@ def test_expansion_inside_retrieval():
                           query_expansion=expansion_op)
     ids = ['fox_ex', 'surf_ex']
     retrieval.fit(DOCUMENTS, ids)
-    result = retrieval.query('surf_ex')
+    result = retrieval.query('surf')
     assert result[0] == 'surf_ex'
     assert result[1] == 'fox_ex'
 
